2498cbcdda0ac70e5b89c7ffcec28a0c7dae1534
Cyberzoneprojects <cyberzone683@gmail.com>
2022-03-18 09:47:01 +0100

    'resolved'

---
+++ src/unitsDashboard/components/ContentCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function HeaderSection(props){
+    return(
+        <>
+             <div className='Unit-header-card Unit-middle-section mt-2 px-0 ml-4'>
+
+             <div className='text_imaeg'>
+                 <div className='middle-section-text'>
+                     <p>LÉGISLATION ET DROIT DU MONDE</p>
+                     <p>CYBER EN FRANCE</p>
+                 </div>
+                 <div className='middle-section-image'>
+                     <img src='./images/biUF-F7i042AZvrL_M0ZugloA4ZIfbq0T-_NOPROCESS_CNIL.png' height='210rem' width="300rem" />
+                 </div>
+             </div>
+
+             </div>
+        </>
+    );
+}
+
+
+export default HeaderSection;

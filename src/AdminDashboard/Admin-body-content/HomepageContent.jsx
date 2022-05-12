e702680d63faa59f10fc8cca648a96a3682aa991
Cyberzoneprojects <cyberzone683@gmail.com>
2022-05-12 15:29:06 +0100

    present

--- src/AdminDashboard/Admin-body-content/HomepageContent.jsx
+++ src/AdminDashboard/Admin-body-content/HomepageContent.jsx
@@ -127,7 +127,7 @@ function HomepageContent (){
 
 
                                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                                  {/* <Chart /> */}
+                                                  <Chart />
                                                 </div>
                                             </div>
                                         </div>

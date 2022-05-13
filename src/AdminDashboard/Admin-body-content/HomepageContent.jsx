7f9d3c9b577a7e8bcdbf2e282b507f3a6f6b1c74
Cyberzoneprojects <cyberzone683@gmail.com>
2022-05-13 06:57:25 +0100

    estelle

--- src/AdminDashboard/Admin-body-content/HomepageContent.jsx
+++ src/AdminDashboard/Admin-body-content/HomepageContent.jsx
@@ -128,6 +128,7 @@ function HomepageContent (){
 
                                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                                   <Chart />
+                                                  
                                                 </div>
                                             </div>
                                         </div>
